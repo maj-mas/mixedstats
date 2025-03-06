2b9630d58a4654c5b1a3b8475feb4f3e6431b0df
Maja Maschke <maschke01@protonmail.com>
2025-03-06 10:41:09 +0100

    confusion

--- src/mix_int_plots.py
+++ src/mix_int_plots.py
@@ -153,7 +153,6 @@ def fug_eqn_min(z, alpha, n, L, a, b, c, d, f):
     grad[0] = 2 * lambda_l2 * z[0] - 2 * f1 * (a + 2*(2*c*L - a**2*L)*z[0] - (a*b + f)*z[1]) + 2 * f2 * (a*b + f)*z[1]
     grad[1] = 2 * lambda_l2 * z[1] + 2 * f1 * ((a*b + f)*z[0]) - 2 * f2 * (b/L + 2*(2*d/L - b**2/L)*z[1] - (a*b + f)*z[0])
 
-    # return np.array([f1, f2])
     return objective, grad
 
 @njit
@@ -169,7 +168,6 @@ def fug_eqn_root(z, alpha, n, L, a, b, c, d, f):
     jac[1, 0] = (a*b + f)*z[1]
     jac[1, 1] = - (b/L + 2 * (2*d/L - b**2/L)*z[1] - (a*b + f)*z[0])
 
-    # return np.array([f1, f2])
     return objective, jac
 
 finish_count = 0
@@ -240,7 +238,13 @@ def fugs(Zc1_L, Zc2_L2, Zq1, Zq2, Zqc2_L):
 @njit
 def p_eos(beta, alpha, n, L, a, b, c, d, f):
     zc = (1-alpha) * n * L * (alpha/(1-alpha)**2 / f - b * f * (f + a)/(f**2 - a*b))
-    zq = (a-alpha) * n * L * (f + a)/(f**2 - a*b)
+    zq = (1-alpha) * n * L * (f + a)/(f**2 - a*b)
+    return 1/(beta) * (a*zc + b/L*zq + 0.5*(2*c*L - a*L**2)*zc**2 + 0.5*(2*d/L - b**2/L)*zq**2 - (a*b + f)*zc*zq) # factors 1/L distributed
+
+@njit 
+def p_eos_z(beta, z, alpha, n, L, a, b, c, d, f):
+    zc = z[0]
+    zq = z[1]
     return 1/(beta) * (a*zc + b/L*zq + 0.5*(2*c*L - a*L**2)*zc**2 + 0.5*(2*d/L - b**2/L)*zq**2 - (a*b + f)*zc*zq) # factors 1/L distributed
 
 def save_zs():
@@ -268,19 +272,19 @@ def save_fugs():
     np.save("errs_root.npy", errs)
 
 def load_fugs():
-    fugs_c = np.load("fugs_c.npy")
-    fugs_q = np.load("fugs_q.npy")
-    errs = np.load("errs.npy")
+    fugs_c = np.load("fugs_root_c.npy")
+    fugs_q = np.load("fugs_root_q.npy")
+    errs = np.load("errs_root.npy")
     return fugs_c, fugs_q, errs
 
-def save_ps():
+def save_ps_z():
     Ts = np.linspace(5e-2, 5, 50)
     gs = [0.01, 0.1, 0.5, 1.0, 2.0]
     Ls = np.logspace(-3, 1, 20)
     alphas = np.linspace(1e-6, 1, 6)
     ns = np.logspace(10, 20, 6)
     Zc1_L, Zc2_L2, Zq1, Zq2, Zqc2_L = load_zs()
-    #fugs_c, fugs_q = load_fugs()
+    fugs_c, fugs_q, err = load_fugs()
 
     ps = np.empty((len(Ts), len(gs), len(Ls), len(alphas), len(ns)))
 
@@ -290,19 +294,55 @@ def save_ps():
                 for l, alpha in enumerate(alphas):
                     for m, n in enumerate(ns):
                         try:
-                            ps[i, j, k, l, m] = p_eos(1/T, 
-                                                  alpha,
-                                                  n,
-                                                  L,
-                                                  Zc1_L[i, j],
-                                                  Zq1[i, j],
-                                                  Zc2_L2[i, j],
-                                                  Zq2[i, j],
-                                                  Zqc2_L[i, j])
+                            zc = fugs_c[i, j, k, l, m]
+                            zq = fugs_q[i, j, k, l, m]
+                            ps[i, j, k, l, m] = p_eos_z(1/T, 
+                                                        [zc, zq],
+                                                        alpha,
+                                                        n,
+                                                        L,
+                                                        Zc1_L[i, j],
+                                                        Zq1[i, j],
+                                                        Zc2_L2[i, j],
+                                                        Zq2[i, j],
+                                                        Zqc2_L[i, j])
+                        except:                            
+                            ps[i, j, k, l, m] = np.nan
+    
+    np.save("ps_z.npy", ps)
+
+def save_ps():
+    Ts = np.linspace(5e-2, 5, 50)
+    gs = [0.01, 0.1, 0.5, 1.0, 2.0]
+    Ls = np.logspace(-3, 1, 20)
+    alphas = np.linspace(1e-6, 1, 6)
+    ns = np.logspace(10, 20, 6)
+    Zc1_L, Zc2_L2, Zq1, Zq2, Zqc2_L = load_zs()
+
+    ps = np.empty((len(Ts), len(gs), len(Ls), len(alphas), len(ns)))
+
+    for i, T in tqdm(enumerate(Ts)):
+        for j, g in enumerate(gs):
+            for k, L in enumerate(Ls):
+                for l, alpha in enumerate(alphas):
+                    for m, n in enumerate(ns):
+                        try:
+                            ps[i, j, k, l, m] = p_eos(1/T,
+                                                        alpha,
+                                                        n,
+                                                        L,
+                                                        Zc1_L[i, j],
+                                                        Zq1[i, j],
+                                                        Zc2_L2[i, j],
+                                                        Zq2[i, j],
+                                                        Zqc2_L[i, j])
                         except:                            
                             ps[i, j, k, l, m] = np.nan
     
     np.save("ps.npy", ps)
+
+def load_ps_z():
+    return np.load("ps_z.npy")
 
 def load_ps():
     return np.load("ps.npy")
@@ -315,6 +355,8 @@ def plot_mus():
     alphas = np.linspace(1e-6, 1, 6)
     ns = np.logspace(10, 20, 6)
     fugs_c, fugs_q, errs = load_fugs()
+    errs = np.abs(errs)
+    errs = np.clip(errs, 1e-3, None)
     
     mus_c = np.empty((len(Ts), len(gs), len(Ls), len(alphas), len(ns)))
     mus_q = np.empty((len(Ts), len(gs), len(Ls), len(alphas), len(ns)))
@@ -334,16 +376,16 @@ def plot_mus():
                 n = ns[m]
                 level_min = min(np.nanmin(fugs_c[:, j, :, l, m]), np.nanmin(fugs_q[:, j, :, l, m]))
                 level_max = max(np.nanmax(fugs_c[:, j, :, l, m]), np.nanmax(fugs_q[:, j, :, l, m]))
-                levels_exp = np.arange(np.floor(np.log10(level_min)-1), np.ceil(np.log10(level_max)+1), step=0.1)
-                levels = np.power(10, levels_exp)
+                #levels_exp = np.arange(np.floor(np.log10(level_min)-1)-1, np.ceil(np.log10(level_max)+1)+1, step=0.1)
+                #levels = np.power(10, levels_exp)
                 level_min_err = np.nanmin(errs[:, j, :, l, m])
                 level_max_err = np.nanmax(errs[:, j, :, l, m])
                 levels_exp_err = np.arange(np.floor(np.log10(level_min_err)-1), np.ceil(np.log10(level_max_err)+1), step=0.1)
                 levels_err = np.power(10, levels_exp_err)
-                #levels = np.linspace(level_min, level_max+1e-6, 100)
+                levels = np.linspace(level_min, level_max+1e-6, 100)
                 fig, [c_ax, q_ax, cb1,  err_ax, cb2] = plt.subplots(figsize=(12, 6), nrows=1, ncols=5, squeeze=True, width_ratios=(1, 1, 0.3, 1, 0.3))
-                cfc = c_ax.contourf(Ts, Ls, fugs_c[:, j, :, l, m].T, levels=levels, norm=LogNorm())
-                cfq = q_ax.contourf(Ts, Ls, fugs_q[:, j, :, l, m].T, levels=levels, norm=LogNorm())
+                cfc = c_ax.contourf(Ts, Ls, fugs_c[:, j, :, l, m].T, levels=levels)#, norm=LogNorm())
+                cfq = q_ax.contourf(Ts, Ls, fugs_q[:, j, :, l, m].T, levels=levels)#, norm=LogNorm())
                 fig.colorbar(cfq, cax=cb1, label="$z$", fraction=0.4)
                 errfq = err_ax.contourf(Ts, Ls, np.abs(errs[:, j, :, l, m].T), levels=levels_err, norm=LogNorm())
                 fig.colorbar(errfq, cax=cb2, label="$f(z)\\overset{!}{=}0$", fraction=0.4)
@@ -352,6 +394,7 @@ def plot_mus():
                 q_ax.set_yscale("log")
                 err_ax.set_yscale("log")
                 q_ax.set_xlabel("$T$ $(\\varepsilon)$")
+                err_ax.set_xlabel("$T$ $(\\varepsilon)$")
                 c_ax.set_ylabel("$L$")
                 c_ax.set_title("$z_\\text{c}$")
                 q_ax.set_title("$z_\\text{q}$")
@@ -373,7 +416,8 @@ def plot_ps():
     Ls = np.logspace(-3, 1, 20)
     alphas = np.linspace(1e-6, 1, 6)
     ns = np.logspace(10, 20, 6)
-    ps = load_ps()
+    ps = load_ps_z()    
+    ps = np.clip(ps, 1e-6, None)
 
     rng = np.random.default_rng()
     random_g_is = rng.integers(len(gs), high=None, size=2)
@@ -387,13 +431,13 @@ def plot_ps():
                 n = ns[m]
                 level_min = np.nanmin(ps[:, j, :, l, m])
                 level_max = np.nanmax(ps[:, j, :, l, m])
-                #levels_exp = np.arange(np.floor(np.log10(level_min)-1)-1, np.ceil(np.log10(level_max)+1)+1, step=0.1)
-                #levels = np.power(10, levels_exp)
-                levels = np.linspace(level_min, level_max, 100)
+                levels_exp = np.arange(np.floor(np.log10(level_min)), np.ceil(np.log10(level_max)), step=0.1)
+                levels = np.power(10, levels_exp)
+                #levels = np.linspace(level_min, level_max, 100)
 
                 fig, ax = plt.subplots()
-                pcf = ax.contourf(Ts, Ls, ps[:, j, :, l, m].T, levels=levels)#, norm=LogNorm())
-                fig.colorbar(pcf, label="$p$ (?)")
+                pcf = ax.contourf(Ts, Ls, ps[:, j, :, l, m].T, levels=levels, norm=LogNorm())
+                fig.colorbar(pcf, label="$p$ (?)", ticks=np.power(10, np.arange(np.floor(np.log10(level_min)), np.ceil(np.log10(level_max)))))
 
                 ax.set_xlabel("$T$ $(\\varepsilon)$")
                 ax.set_yscale("log")
@@ -403,13 +447,14 @@ def plot_ps():
                 ax.set_ylim(1e-3, 10)
                 fig.suptitle(f"$g={g}\\,\\varepsilon$, $\\alpha={alpha}$, $n={np.format_float_scientific(n, precision=3)}/L$")
 
-                #fig.tight_layout()
+                fig.tight_layout()
                 fig.savefig(f"../plots/int-mix/ps/p_g{g}_alpha{alpha}_n{n}.pdf")
                 plt.close(fig)
 
 
 # save_zs()
-save_fugs()
+# save_fugs()
 # save_ps()
-# plot_ps()
-plot_mus()
+# save_ps_z()
+plot_ps()
+# plot_mus()
